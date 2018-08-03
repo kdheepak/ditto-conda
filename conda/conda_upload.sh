52ef09471d9ee0634525eb97d5f5898b0ae8ce90
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-08-03 16:15:29 -0600

    Build for Windows from MacOS

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -7,4 +7,8 @@ conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION="0.1.0"
 conda build $PKG_NAME
+if [ "$(uname)" == "Darwin" ]; then
+  conda convert $CONDA_BLD_PATH/$OS/$PKG_NAME-*.tar.bz2 -p win-32
+  conda convert $CONDA_BLD_PATH/$OS/$PKG_NAME-*.tar.bz2 -p win-64
+fi
 anaconda -t $CONDA_UPLOAD_TOKEN upload $CONDA_BLD_PATH/$OS/$PKG_NAME-*.tar.bz2 --force
